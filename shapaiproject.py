91eca0174719da67a1d5452dac1c348d0849c1eb
siri1904 <86296477+siri1904@users.noreply.github.com>
2021-06-22 17:19:40 +0530

    ok

---
+++ shapaiproject.py
@@ -0,0 +1,36 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = '1f4222114e239c77af03764e4e233e6f'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city =((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+with open('weather record.txt', 'w') as f:
+    f.writelines(location)
+with open('weather record.txt', 'a') as f:
+    f.writelines('\n'.join(weather_desc))
+with open('weather record.txt', 'a') as f:
+    f.writelines('\n'.join(temp_city))
+with open('weather record.txt', 'a') as f:
+    f.writelines('\n'.join(hmdt))
+with open('weather record.txt', 'a') as f:
+    f.writelines('\n'.join(wind_spd))
